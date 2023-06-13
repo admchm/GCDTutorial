565e1785c100d744d817f4632cf0746aeb863a09
Adam Chomicki <admchm@outlook.com>
2023-06-13 09:51:13 +0200

    Implement functionality for downloading images

--- #4 Groups and semaphores/Images.playground/Contents.swift
+++ #4 Groups and semaphores/Images.playground/Contents.swift
@@ -40,14 +40,29 @@ let group = DispatchGroup()
 let queue = DispatchQueue.global(qos: .userInitiated)
 
 let base = "https://wolverine.raywenderlich.com/books/con/image-from-rawpixel-id-"
+let fileExtension = "-jpeg.jpg"
 let ids = [ 466881, 466910, 466925, 466931, 466978, 467028, 467032, 467042, 467052 ]
 
 var images: [UIImage] = []
 
-// TODO: Implement required functionality
-// group.enter()
-// let task = URLSession.shared..
-// defer
-// task.resume()
-// group.notify(queue: queue)
-// PlaygroundPage.current.finishExecution()
+for id in ids {
+    guard let url = URL(string: "\(base)\(id)\(fileExtension)") else { continue }
+    
+    group.enter()
+    let task = URLSession.shared.dataTask(with: url) { data, _, error in
+        
+        defer { group.leave() }
+        
+        if error == nil, let data = data, let image = UIImage(data: data) {
+            images.append(image)
+        }
+    }
+    
+    task.resume()
+}
+
+group.notify(queue: queue) {
+    images[0]
+    print("images.count: \(images.count)")
+    PlaygroundPage.current.finishExecution()
+}
