2a89fb0e9c17f0e4135c29d07327c4f25af5ab3a
Adam Chomicki <admchm@outlook.com>
2023-06-19 15:17:18 +0200

    Create project for GCD and async/await test

---
+++ #13 GCD and Async:Await test/GCDAndAsyncAwait/GCDAndAsyncAwait/GCDTestViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GCDTestViewController.swift
+//  GCDAndAsyncAwait
+//
+//  Created by Adam Chomicki on 19/06/2023.
+//
+
+import UIKit
+
+class GCDTestViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.isHidden = false
+    }
+}
